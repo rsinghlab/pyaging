0e438905872f51ea3295b249ffd9adcd10a480ee
Lucas Camillo <lucascamillo@Lucass-MacBook-Pro-4.local>
2023-11-24 20:41:06 +0000

    changed readme

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -35,7 +35,7 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 
 html_theme = 'sphinx_rtd_theme'
 html_static_path = ['../_static']
-html_logo = '../_static/logo.png'
+html_logo = 'https://pyaging.readthedocs.io/en/latest/_static/logo.png'
 html_css_files = [
     'custom.css',
 ]
