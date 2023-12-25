8c277c063ecc995032b5eb6a598851cb21445e00
Lucas Camillo <lucascamillo@Lucass-MacBook-Pro-4.local>
2023-12-25 13:51:29 -0300

    updated docs theme

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -8,12 +8,15 @@
 
 import os
 import sys
+from datetime import datetime
+from pathlib import Path
+from importlib import metadata
 sys.path.insert(0, os.path.abspath('../../'))  # Adjust the path as needed
 
 project = 'pyaging'
 copyright = '2023, Lucas Paulo de Lima Camillo'
 author = 'Lucas Paulo de Lima Camillo'
-release = '0.0.9'
+release = version = metadata.version("pyaging")
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
@@ -24,22 +27,45 @@ extensions = [
     "sphinx.ext.viewcode",
     "nbsphinx",
     "nbsphinx_link",
+    "myst_parser",
+    "sphinx_copybutton",
+    "sphinx.ext.intersphinx",
+    "sphinx.ext.doctest",
+    "sphinx.ext.coverage",
+    "sphinx.ext.mathjax",
+    "sphinx.ext.autosummary",
+    "sphinx_autodoc_typehints",  # needs to be after napoleon
+    "sphinx_issues",
+    "sphinx_design",
+    "scanpydoc",  # needs to be before linkcode
+    "sphinx.ext.linkcode",
+    "IPython.sphinxext.ipython_console_highlighting",
 ]
-templates_path = ['../_templates']
-exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 
+templates_path = ['../_templates']
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store', "**.ipynb_checkpoints", "tutorials/notebooks/*.rst"]
+html_static_path = ['../_static']
+source_suffix = [".rst", ".md"]
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'sphinx_rtd_theme'
-html_static_path = ['../_static']
+html_theme = 'sphinx_book_theme'
+html_theme_options = dict(
+    use_repository_button=True,
+    repository_url="https://github.com/rsinghlab/pyaging",
+    repository_branch="main",
+    navigation_with_keys=False,  # https://github.com/pydata/pydata-sphinx-theme/issues/1492
+)
 html_logo = '../_static/logo.png'
-html_css_files = [
-    'custom.css',
-]
+html_css_files = ['custom.css']
 
 # -- Options for nbshpinx ----------------------------------------------------
 # https://nbsphinx.readthedocs.io/en/0.8.0/configure.html
 
 nbsphinx_execute = "never"
+
+# -- Additional configurations from the provided conf.py ----------------------
+
+# ... (Add other configurations from the provided conf.py here)
+# Make sure to resolve any conflicts with the existing settings above.
